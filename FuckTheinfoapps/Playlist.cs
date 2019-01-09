00740fbd8f087ac58d50839bd721d6ef497659b0
zzz <40310207+xorpl@users.noreply.github.com>
2019-01-09 10:49:58 +0900

    Add files via upload

--- FuckTheinfoapps/Playlist.cs
+++ FuckTheinfoapps/Playlist.cs
@@ -2,37 +2,13 @@
 {
     class Playlist
     {
-        private string pName;
-        private string pUrl;
+        public string Name;
+        public string Url;
 
         public Playlist(string _Name, string _Url)
         {
-            pName = _Name;
-            pUrl = _Url;
-        }
-
-        public string Name
-        {
-            get
-            {
-                return pName;
-            }
-            set
-            {
-                pName = value;
-            }
-        }
-
-        public string Url
-        {
-            get
-            {
-                return pUrl;
-            }
-            set
-            {
-                pUrl = value;
-            }
+            Name = _Name;
+            Url = _Url;
         }
     }
 }
